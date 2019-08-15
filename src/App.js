53b170061f1faeaa473ccacb460d716789b7a135
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-15 18:23:00 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: 'Vitor'
+      name: 'Vitor',
+      email: 'vms'
     }
     this.changeState = this.changeState.bind(this)
     this.resetState = this.resetState.bind(this)
+    this.changeInput = this.changeInput.bind(this)
+
 
   }
 
@@ -22,22 +25,51 @@ export default class App extends Component {
     this.setState({ name: 'Vitor ' })
   }
 
+  changeInput(event) {
+    let target = event.target
+    let index = target.name
+    this.setState({
+      [index]: target.value
+    })
+  }
+
   render() {
     return (
       <div>
-      <div className="App">
-        <header className="App-header">
-          
-        {/* 
-          <p>
-            {this.state.name}
-          </p>
-          <button onClick={this.changeState}>Mudar estado</button>
-          <button onClick={this.resetState}>Reset estado</button>
-          */}
-        </header>
-      </div>
-      <Vitor></Vitor>
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <form action="">
+                <label >
+                  Nome: <br />
+                  <input
+                    type="text"
+                    name='name'
+                    value={this.state.name}
+                    onChange = {this.changeInput}
+                  />
+                </label><br />
+                <label >
+                  Email: <br />
+                  <input
+                    type="text"
+                    name='email'
+                    value={this.state.email}
+                    onChange = {this.changeInput}
+
+                  />
+                </label>
+              </form>
+            </div>
+            <p>
+              {this.state.name} - {this.state.email}
+            </p>
+            <button onClick={this.changeState}>Mudar estado</button>
+            <button onClick={this.resetState}>Reset estado</button>
+
+          </header>
+        </div>
+        <Vitor></Vitor>
       </div>
     );
   }

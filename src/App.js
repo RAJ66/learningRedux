0b3505f661defff129b104ba01d32a8f5c99216e
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-24 14:59:17 +0100

    add page home and user config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import {Link, Route} from 'react-router-dom';
-
+import { Link, Route } from 'react-router-dom';
+import routerConfig from './RoutesConfig'
 import './App.css';
 
 
 export default class App extends Component {
- 
+
   render() {
     return (
+      <div>
         <div className="App">
           <Link to="/">Home</Link>
           <Link to="/user">User</Link>
 
-        </div>    
+        </div>
+        {routerConfig.map((value, key) => {
+          return <Route
+            key={key}
+            path={value.path}
+            component={value.component}
+            exact={value.exact}
+          ></Route>
+        })}
+      </div>
     );
   }
 }

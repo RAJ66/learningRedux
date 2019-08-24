67f5f60df5e5d2dca12f01ed7c6af7b062a09e7b
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-24 14:34:54 +0100

    add page home and user

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+export default function Home() {
+
+
+  return (
+    <div >
+     <h1>Pagina Inicial 😁</h1> 
+    </div>
+
+  )
+
+}
---
+++ src/pages/user/User.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+
+export default function User() {
+  let list = [
+    {
+      name: 'Vitor',
+      email: 'vitor@gmail.com'
+    },
+    {
+      name: 'tiago',
+      email: 'tiago@gmail.com'
+    },
+  ]
+
+  return (
+    <div  className='tableUser'>
+      <table className='listUser' >
+        <tr>
+          <th className='listUser'>Name:</th>
+          <th className='listUser'>Email:</th>
+
+        </tr>
+        {list.map((item)=>{
+          return(
+          <tr>
+            <td className='listUser'>{item.name}</td>
+            <td className='listUser'>{item.email}</td>
+          
+          </tr>)
+        })}
+      </table>
+    </div>
+
+  )
+
+}

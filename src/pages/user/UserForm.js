c99aea81fbcdb904ce6c882e64713052564c87eb
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-31 17:42:20 +0100

    button styles

--- src/pages/user/UserForm.js
+++ src/pages/user/UserForm.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import {reduxForm,Field} from 'redux-form'
-import {connect} from 'react-redux'
-import {submitUserAction} from '../../actions/user/UserAction'
+import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { submitUserAction } from '../../actions/user/UserAction'
 
-const UserFormFunc = props =>{
-const {handleSubmit} = props
+const UserFormFunc = props => {
+  const { handleSubmit } = props
 
-const submit = (data,submitUserAction)=>{
-  submitUserAction(data)
-}
+  const submit = (data, submitUserAction) => {
+    submitUserAction(data)
+  }
 
-  return(
-    <form onSubmit={handleSubmit((fields)=>submit(fields,submitUserAction))} >
+  return (
+    <form onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))} >
       <label htmlFor="">
         Name:
-      </label><br/>
-      <Field type="text" component='input' name='name'/>
-<br/>
+      </label><br />
+      <Field type="text" component='input' name='name' />
+      <br />
       <label htmlFor="">
         Email:
-      </label><br/>
-      <Field type="text" component='input' name='email'/>´
-      <button type='submit'>Submit</button>
+      </label><br />
+      <Field type="text" component='input' name='email' />
+      <br/>
+      <button type='submit' className='buttonForm'>Submit</button>
     </form>
   )
 
 }
 
 const UserForm = (reduxForm({
-  form:'formUser'
+  form: 'formUser'
 }))(UserFormFunc)
 
-const mapStateToProps = state =>({})
+const mapStateToProps = state => ({})
 
-export default connect(mapStateToProps,{submitUserAction})(UserForm)
+export default connect(mapStateToProps, { submitUserAction })(UserForm)

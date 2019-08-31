31ad5fd7b948caf5ecba64491cfb89efbfe98e55
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-31 17:56:19 +0100

    bug height complet

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className='Total'>
+
         <div className="App">
           <Link className='page' to="/">Home</Link>
           <Link className='page' to="/user">User</Link>
-
         </div>
-        {routerConfig.map((value, key) => {
-          return <Route
-            key={key}
-            path={value.path}
-            component={value.component}
-            exact={value.exact}
-          ></Route>
-        })}
+        <div className='Main'>
+          {routerConfig.map((value, key) => {
+            return <Route
+              key={key}
+              path={value.path}
+              component={value.component}
+              exact={value.exact}
+            ></Route>
+          })}
+        </div>
       </div>
     );
   }

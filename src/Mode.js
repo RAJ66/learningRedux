8270a595703d3b2414aa814555a7ba283d42d4f4
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-02 18:56:21 +0100

    alteration colors

--- src/Mode.js
+++ src/Mode.js
@@ -1,14 +1,15 @@
 function lightMode() {
   console.log('light mode : on');
-  document.getElementById("mainTest").style.backgroundColor = "rgb(242,231,254)";
-  document.getElementById("home").style.color = "black";
+  document.getElementById("mainTest").style.backgroundColor = "#faf9fa";
+  document.getElementById("home").style.color = "#19171C";
 
 };
 
 function darkMode(){
   console.log('dark mode : on');
-  document.getElementById("mainTest").style.backgroundColor = "black";
-  document.getElementById("home").style.color = "white";
+  document.getElementById("mainTest").style.backgroundColor = "#0f0e11";
+  document.getElementById("home").style.color = "#dad8de";
 };
 
 export { lightMode,darkMode};
+

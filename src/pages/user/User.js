de031f34c97dada458dc2aa863570f5b1b6c2c6d
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-09-08 18:34:56 +0100

    added eslint

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,56 +1,45 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 import UserForm from './UserForm';
-import photoVitor from '../../photos/vitor.jpg'
-import photoTiago from '../../photos/tiago.jpg'
-
-
-
-
+import photoVitor from '../../photos/vitor.jpg';
+import photoTiago from '../../photos/tiago.jpg';
 
 export default function User() {
-  let list = [
+  const list = [
     {
       name: 'Vitor',
       email: 'vitor@gmail.com',
-      img: photoVitor
+      img: photoVitor,
     },
     {
       name: 'tiago',
       email: 'tiago@gmail.com',
-      img: photoTiago
+      img: photoTiago,
     },
-  ]
+  ];
 
   return (
-    <div className='Home' id='home'>
-      <div className='customForm' >
-        <UserForm></UserForm>
-
+    <div className="Home" id="home">
+      <div className="customForm">
+        <UserForm />
       </div>
       <br />
-      <table className='customTable' >
-        <tr >
-          <th >Name:</th>
-          <th >Email:</th>
-          <th >Photo:</th>
-
-
+      <table className="customTable">
+        <tr>
+          <th>Name:</th>
+          <th>Email:</th>
+          <th>Photo:</th>
         </tr>
-        {list.map((item) => {
-          return (
-            <tr>
-              <td >{item.name}</td>
-              <td >{item.email}</td>
-              <td className='tdImg'>
-                <img src={item.img} />
-              </td>
-
-            </tr>)
-        })}
+        {list.map((item) => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td className="tdImg">
+              <img src={item.img} alt="user picture" aria-hidden="true" />
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
-
-  )
-
+  );
 }
--- src/pages/user/UserForm.js
+++ src/pages/user/UserForm.js
@@ -1,37 +1,38 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form'
-import { connect } from 'react-redux'
-import { submitUserAction } from '../../actions/user/UserAction'
+import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { submitUserAction } from '../../actions/user/UserAction';
 
-const UserFormFunc = props => {
-  const { handleSubmit } = props
+const UserFormFunc = (props) => {
+  const { handleSubmit } = props;
 
   const submit = (data, submitUserAction) => {
-    submitUserAction(data)
-  }
+    submitUserAction(data);
+  };
 
   return (
-    <form onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))} >
-      <label htmlFor="">
-         Name:
-      </label>
-      <Field type="text" component='input' name='nome' className='inputForm' />
-     <br/><br/>
-      <label htmlFor="">
-        Email:
-      </label>
-      <Field type="text" component='input' name='email' className='inputForm'/>
-      <br/>
-      <button type='submit' className='buttonForm'>Submit</button>
+    <form onSubmit={handleSubmit((fields) => submit(fields, submitUserAction))}>
+      <label htmlFor="">Name:</label>
+      <Field type="text" component="input" name="nome" className="inputForm" />
+      <br />
+      <br />
+      <label htmlFor="">Email:</label>
+      <Field type="text" component="input" name="email" className="inputForm" />
+      <br />
+      <button type="submit" className="buttonForm">
+        Submit
+      </button>
     </form>
-  )
+  );
+};
 
-}
+const UserForm = reduxForm({
+  form: 'formUser',
+})(UserFormFunc);
 
-const UserForm = (reduxForm({
-  form: 'formUser'
-}))(UserFormFunc)
+const mapStateToProps = (state) => ({});
 
-const mapStateToProps = state => ({})
-
-export default connect(mapStateToProps, { submitUserAction })(UserForm)
+export default connect(
+  mapStateToProps,
+  { submitUserAction },
+)(UserForm);
